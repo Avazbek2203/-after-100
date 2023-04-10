679b9b4266a27c90db76a65eee69e6f4fe7b3f6c
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-10 10:33:28 +0500

    Add project files.

---
+++ Revision 1.0/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Revision_1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number : ");
+            string input = Console.ReadLine();
+            string result = "";
+
+            for (int i = input.Length; i > 0; i--)
+            {
+                if (i % 3 == 0 && i != input.Length)
+                {
+                    result += ",";
+                }
+                result += input[input.Length - i];
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
